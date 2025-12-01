bittencourtjulival/android_device_xiaomi_stone

stone: Define missing HALs in FCM
The following HALs in device manifest are not declared in FCM <= level 6:
   android.hardware.biometrics.fingerprint.IFingerprint/default (@4)
   android.hardware.bluetooth.audio.IBluetoothAudioProviderFactory/default (@4)
   android.hardware.drm.IDrmFactory/clearkey (@1)
   android.hardware.health.IHealth/default (@3)
   android.hardware.ir.IConsumerIr/default (@1)
   android.hardware.power.IPower/default (@5)
   android.hardware.sensors.ISensors/default (@2)
   android.hardware.thermal.IThermal/default (@1)
   android.hardware.usb.IUsb/default (@1)
   android.hardware.wifi.IWifi/default (@2)
   android.hardware.wifi.hostapd.IHostapd/default (@2)
   android.hardware.wifi.supplicant.ISupplicant/default (@3)
COMPATIBLE

BoardConfig.mk
@@ -62,7 +62,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/device_framework_matrix.xml
+    
 
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/manifest.xml

Project-Astraverse/device_xiaomi_stone

BoardConfig.mk
@@ -65,7 +65,8 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/vintf/device_framework_matrix.xml
+    $(DEVICE_PATH)/vintf/device_framework_matrix.xml \
+    $(DEVICE_PATH)/vintf/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/vintf/manifest.xml